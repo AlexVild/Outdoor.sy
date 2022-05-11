d57ab4115813c516f014654e9e57c10b3541e1ff
AlexVild <ajv2324@gmail.com>
2022-05-11 19:31:23 -0400

    feat: (AV) Cleanup sort UI

--- src/app/customer-table/customer-table.component.ts
+++ src/app/customer-table/customer-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { Customer } from './customer';
 
