f21191478165f8ec1c2f8d34de1072905a9588df
AlexVild <ajv2324@gmail.com>
2022-05-11 13:40:42 -0400

    test: (AV) - Added tests for customer component

--- src/app/customer-table/customer-table.component.spec.ts
+++ src/app/customer-table/customer-table.component.spec.ts
@@ -3,23 +3,47 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomerTableComponent } from './customer-table.component';
 
 describe('CustomerTableComponent', () => {
-  let component: CustomerTableComponent;
-  let fixture: ComponentFixture<CustomerTableComponent>;
+	let component: CustomerTableComponent;
+	let fixture: ComponentFixture<CustomerTableComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CustomerTableComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ CustomerTableComponent ]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CustomerTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CustomerTableComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('the customer table', () => {
+		it('should not be visible when there are no valid customers loaded into the component', () => {
+			const noCustomerElement = fixture.nativeElement.querySelector('.no-customers-message');
+			const customerTable = fixture.nativeElement.querySelector('.customer-table');
+
+			expect(noCustomerElement).toBeTruthy();
+			expect(customerTable).toBeFalsy();
+		});
+
+		it('should be visible when customers are given to the component', () => {
+			const noCustomerElement = fixture.nativeElement.querySelector('.no-customers-message');
+			const customerTable = fixture.nativeElement.querySelector('.customer-table');
+
+			expect(customerTable).toBeTruthy();
+			expect(noCustomerElement).toBeFalsy();
+		});
+
+		it('should display an error message when no customers are loaded into the component', () => {
+			const noCustomerElement = fixture.nativeElement.querySelector('.no-customers-message');
+
+			expect(noCustomerElement.textContent).toContain('You must first upload a customer list to view customer data.');
+		});
+	});
 });
